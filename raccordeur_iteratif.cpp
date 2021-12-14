132b4c9e916de2a1b487933188c610ae44bb0d42
Florian <florian.suchaud@hotmail.fr>
2021-12-14 14:40:52 -0500

    Commentaire

--- raccordeur_iteratif.cpp
+++ raccordeur_iteratif.cpp
@@ -4,13 +4,18 @@ int RaccordeurIteratif::calculerRaccord(MatInt2* distances, int* coupe){
     int hauteur = distances->nLignes();
     int largeur = distances->nColonnes();
     MatInt2 E = MatInt2(hauteur,largeur);
+    //E est la valeur minimale de l'erreure cumulative de ce pixel.
 
     int minI= 0;
     for(int i=0;i<largeur;i++){
         E.set(0,i,distances->get(0,i));
     }
     int masterMin;
+    //pour chaque hauteur
     for (int i=1;i<hauteur;i++){
+      //et pour chaque cases de ces hauteurs on calcule la valeur de E, qui est le
+      //minimum des 3 cases de la hauteur - 1 qui sont en contact avec la case en cours de
+      //traitement.
         for(int j=0;j<largeur;j++){
             if(j==0){
                 masterMin=min(E.get(i-1,j),E.get(i-1,j+1));
@@ -31,6 +36,8 @@ int RaccordeurIteratif::calculerRaccord(MatInt2* distances, int* coupe){
     }
 
 
+    //pour chaque hauteur à partir de chaque dernière coupe ajoutée, on calcule
+    //l'indice de la coupe liée à cette hauteur puis on l'ajoute au tableau
     for(int j=hauteur-2;j>=0;j--){
         if(minI==0){
             if(E.get(j,minI)<E.get(j,minI+1)){
--- raccordeur_recursif.cpp
+++ raccordeur_recursif.cpp
@@ -5,6 +5,7 @@ int RaccordeurRecursif::calculerRaccord(MatInt2* distances, int* coupe){
 
     int hauteur = distances->nLignes();
     int largeur = distances->nColonnes();
+
     MatInt2 E = MatInt2(hauteur,largeur);
     calcul_Recursif(distances,coupe, hauteur-1,&E);
     int minI= 0;
@@ -14,6 +15,8 @@ int RaccordeurRecursif::calculerRaccord(MatInt2* distances, int* coupe){
             continue;
         minI = i;
     }
+    //pour chaque hauteur à partir de chaque dernière coupe ajoutée, on calcule
+    //l'indice de la coupe liée à cette hauteur puis on l'ajoute au tableau
     for(int j=hauteur-2;j>=0;j--){
         if(minI==0){
             if(E.get(j,minI)<E.get(j,minI+1)){
@@ -43,12 +46,17 @@ int RaccordeurRecursif::calculerRaccord(MatInt2* distances, int* coupe){
 
 void calcul_Recursif(MatInt2* distances, int* coupe, int indices, MatInt2* E){
     int largeur=distances->nColonnes();
+    //si on est à la première ligne de notre image, on ne peut pas utiliser les valeurs de la ligne précédante
+    //donc on initialise la valeur de E à la valeur de la distance
     if(indices==0){
         for(int i=0;i<largeur;i++){
             E->set(0,i,distances->get(0,i));
         }
     }
+    //sinon on ajoute la valeur mininale des trois valeurs de l'étage inférieur sous la cellule avec
+    //la valeur de la distance
     else{
+        //on calcule la hauteur inférieur
         calcul_Recursif(distances,coupe,indices-1,E);
         for(int i=0;i<largeur;i++){
             int masterMin;
